bc40362f1944ca690a9e010cad0745d92025ce53
Holger Pieta <pieta@physik.rwth-aachen.de>
2010-11-16 14:53:36 +0100

    Working around a weird C++ effect
    For some reasons, (part->getPt() == part->getPt()) is occasionally false and in this cases other operators behave oddly, too.
    To avoid that, store the results first in variables, then use them.

--- Tools/PXL/Sort.hh
+++ Tools/PXL/Sort.hh
@@ -10,7 +10,10 @@
 namespace pxl {
    //inline to avoid stupid compiler errors
    inline bool compPart( pxl::Particle *first, pxl::Particle *second ) {
-      return first->getPt() > second->getPt();
+      //there might be something weird going on when doing a direct comparision, so store the values in variables first
+      double pt1 = first->getPt();
+      double pt2 = second->getPt();
+      return pt1 > pt2;
    }
 
    inline void sortParticles( std::vector< pxl::Particle* > &vec ) {
